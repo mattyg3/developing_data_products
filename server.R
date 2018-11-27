2977902bcbdde307f81238587bd6b1aa17d33358
Matt Goldsmith <surfff4life@gmail.com>
2018-11-26 19:30:24 -0800

    second

--- server.R
+++ server.R
@@ -1,12 +1,22 @@
 library(shiny)
-library(ggplot2)
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+    
+    output$plot <- renderPlot({
         dd <- rnorm(input$obs)
-        hist(dd)
-  })
-  
+        {hist(dd, col = "blue", main = "Normal Distribution")
+            abline(v=mean(dd), col="red")}
+    })
+    
+    output$doc <- renderPrint({
+        a <- "To use this tool, simply select the number of observations in the slider on the left and the tool will automatically run a random normal distribution. The red line is the mean of the random data" 
+        a
+    })
+    output$calc <- renderPrint({
+        b <- "Calculation performed to create plot: dd <- rnorm(input$obs); {hist(dd, col = blue, main = Normal Distribution); abline(v=mean(dd), col=red)}"
+        b
+    })
+    
 })
--- ui.R
+++ ui.R
@@ -2,23 +2,28 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Random Data"),
-  
-  # Sidebar with a slider input for number of observations
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("obs",
-                   "Number of Observations for random histogram:",
-                   min = 1,
-                   max = 1000,
-                   value = 500)
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+    # Application title
+    titlePanel("Random Data"),
+    
+    # Sidebar with a slider input for number of observations
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("obs",
+                        "Number of Observations:",
+                        min = 1,
+                        max = 5000,
+                        value = 1000)
+        ),
+        
+        # Main Panel w/ tabs
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        tabPanel("Plot", plotOutput("plot")),
+                        tabPanel("Documentation", verbatimTextOutput("doc")),
+                        tabPanel("Calculation", verbatimTextOutput("calc"))
+                        
+            )
+        )
+    ))
+)
